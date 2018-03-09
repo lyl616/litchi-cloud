0dc152465e168e4713eb8c5c205e96935b6704cf
liuyulong <liuyulong@wamingtech.com>
2018-03-09 10:07:14 +0800

    添加 getway-zuul-reg

---
+++ litchi-gateway-zuul-reg-exp/src/main/java/org/litchi/cloud/zuul/LitchiGatewayZullRgeExpApplication.java
@@ -0,0 +1,20 @@
+package org.litchi.cloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class LitchiGatewayZullRgeExpApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LitchiGatewayZullRgeExpApplication.class, args);
+	}
+
+	@Bean
+	public PatternServiceRouteMapper serviceRouteMapper() {
+		return new PatternServiceRouteMapper("(?<name>^.+)-(?<version>v.+$)", "${version}/${name}");
+	}
+}
