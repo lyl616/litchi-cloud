0e5329da89c1cf59e5fde80a7d1bc25c3f421f0e
liuyulong <liuyulong@wamingtech.com>
2018-03-16 22:03:03 +0800

    zuul-filter

--- litchi-gateway-zuul-filter/src/main/java/org/litchi/cloud/zuul/PreZuulFilter.java
+++ litchi-gateway-zuul-filter/src/main/java/org/litchi/cloud/zuul/PreZuulFilter.java
@@ -1,31 +1,39 @@
- package org.litchi.cloud.zuul;
+package org.litchi.cloud.zuul;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 
 public class PreZuulFilter extends ZuulFilter {
 
+	private Logger logger = LoggerFactory.getLogger(PreZuulFilter.class);
+
 	@Override
 	public Object run() {
-		// TODO Auto-generated method stub
+		HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+		String remoteHost = request.getRemoteHost();
+		String name = request.getServerName();
+		logger.info("请求的接口host:" + remoteHost+"      请求的接口名称:" + name);
 		return null;
 	}
 
 	@Override
 	public boolean shouldFilter() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public int filterOrder() {
-		// TODO Auto-generated method stub
 		return 1;
 	}
 
 	@Override
 	public String filterType() {
-		// TODO Auto-generated method stub
-		return "pre" ;
+		return "pre";
 	}
 
 }
--- litchi-gateway-zuul-filter/src/main/java/org/litchi/cloud/zuul/ZuulFilterApplication.java
+++ litchi-gateway-zuul-filter/src/main/java/org/litchi/cloud/zuul/ZuulFilterApplication.java
@@ -3,11 +3,17 @@ package org.litchi.cloud.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication 
+@SpringBootApplication
 @EnableZuulProxy
-public class LitchiZuulFilterApplication {
+public class ZuulFilterApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(LitchiZuulFilterApplication.class, args);
+		SpringApplication.run(ZuulFilterApplication.class, args);
+	}
+
+	@Bean
+	public PreZuulFilter PreZuulFilter() {
+		return new PreZuulFilter();
 	}
 }
